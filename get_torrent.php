5f608a0fb1fbdc941a5216a6e560db27f4da80a2
Simon Haga <1498414+sewil@users.noreply.github.com>
2019-07-13 21:08:33 +0200

    Remove episode param from get torrent

--- get_torrent.php
+++ get_torrent.php
@@ -1,24 +1,11 @@
 <?php
 require_once 'RequestHeader.php';
-require_once 'db_context.php';
 include_once 'torrent_utils.php';
-$torrents = TorrentUtils::getTorrents();
-if(isset($_GET['episode'])) {
-	$context = new db_context();
-	$context->connect();
-	$episode = $context->read_episode($_GET['episode']);
-	$context->disconnect();
-	if($episode == null) {
-		echo "Episode not found";
-		exit;
-	} else {
-		$torrent_hash = $episode["torrent_hash"];
-	}
-} else if(isset($_GET['torrent_hash'])) {
-	$torrent_hash = $_GET['torrent_hash'];
-}
 
-if($torrent_hash == null || strlen($torrent_hash) == 0) {
+$torrents = TorrentUtils::getTorrents();
+if(isset($_GET['torrent_hash'])) {
+	$torrent_hash = $_GET['torrent_hash'];
+} else if($torrent_hash == null || strlen($torrent_hash) == 0) {
 	echo "No torrent hash found.";
 	exit;
 }
