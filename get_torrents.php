0c46e060b253b7d9a52b672b66adc0864843b65d
Simon Haga <1498414+sewil@users.noreply.github.com>
2020-05-24 17:04:52 +0200

    Create get_torrents.php

---
+++ get_torrents.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'RequestHeader.php';
+require_once 'db_context.php';
+include_once 'string_utils.php';
+include_once 'secure_indexer.php';
+include_once 'torrent_utils.php';
+
+$context = new db_context();
+$context->connect();
+$arcs = $context->list_arcs();
+$episodes = $context->list_episodes();
+$context->disconnect();
+$data = [];
+$ftp_torrents = TorrentUtils::getTorrents();
+$torrents = [];
+
+foreach($arcs as $arc) {
+	if ($arc['released'] !== 1) {
+		continue;
+	}
+	$torrent = TorrentUtils::findTorrent($ftp_torrents, $arc['torrent_hash']);
+	if ($torrent != null) {
+		$torrents[] = $torrent;
+	}
+}
+foreach($episodes as $episode) {
+	$is_released = isset($episode["released_date"]) && strtotime($episode["released_date"]) <= time();
+	if (!$is_released) {
+		continue;
+	}
+	$torrent = TorrentUtils::findTorrent($ftp_torrents, $episode['torrent_hash']);
+	if ($torrent != null) {
+		$torrents[] = $torrent;
+	}
+}
+
+foreach ($torrents as $torrent) {
+	$data['torrents'][] = [
+		'age_days' => $torrent['age_days'],
+		'hash' => $torrent['hash'],
+		'trackers' => $torrent['trackers'],
+		'magnet' => $torrent['magnet'],
+		'torrent_name' => $torrent['torrent_name'],
+		'display_name' => $torrent['display_name'],
+		'size_raw' => $torrent['size_raw'],
+		'size' => $torrent['size'],
+		'created' => $torrent['created'],
+		'created_raw' => $torrent['created_raw']
+	];
+}
+echo json_encode($data);
+?>
